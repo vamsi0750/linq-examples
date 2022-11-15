afc1453504b846e8143d2e5a12ed5b3b77f6d6d3
vamsi krishna <vamsi.n@edifybrokers.com>
2022-11-15 16:39:02 +0530

    added all and any method examples

---
+++ Any/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Any
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //We can use any in two ways
+            // 1. to check collection or data source has any data or not
+            // 2. check whether at least one of the elements of a data source satisfies a given condition or not
+
+            //1
+            int[] IntArray = { 11, 22, 33, 44, 55 };
+            var ResultMS = IntArray.Any();  //return true
+
+
+            //2
+            int[] IntArray2 = { 11, 22, 33, 44, 55 };
+            var ResultMS2 = IntArray2.Any(x=>x > 5);  //return true
+
+            //2
+            int[] IntArray3 = { 11, 22, 33, 44, 55 };
+            var ResultMS3 = IntArray3.Any(x => x > 100);  //return false
+
+        }
+    }
+}
---
+++ all/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace all
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] IntArray = { 11, 22, 33, 44, 55};
+            var Result = IntArray.All(x => x > 10);
+            Console.WriteLine("Is All Numbers are greater than 10 : " + Result); // true
+
+            int[] IntArray2 = { 11, 22, 33, 44,5, 55 };
+            var Result2 = IntArray2.All(x => x > 10);
+            Console.WriteLine("Is All Numbers are greater than 10 : " + Result2); // false
+
+
+            //checking multiple conditions
+            var Result3 = IntArray2.All(x => x > 1 && x > 2);
+            Console.WriteLine("Is All Numbers are greater than 10 : " + Result3); // false
+
+
+
+        }
+    }
+}
