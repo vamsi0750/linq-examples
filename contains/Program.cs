6d959badef16beee3f4c773e1ec7382dc12298d0
vamsi krishna <vamsi.n@edifybrokers.com>
2022-11-17 12:35:25 +0530

    added contains

---
+++ contains/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+namespace contains
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //contain will check collection or data source specfied element is there or not 
+
+            int[] IntArray = { 11, 22, 33, 44, 55 };
+
+            var IsExistsMS = IntArray.Contains(33); // true
+
+            bool is1Exist = IntArray.AsEnumerable().Contains(1);// false
+
+
+
+        }
+    }
+}
